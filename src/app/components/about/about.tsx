644559b5c4e2788faffac4ef5a47040a40a0c804
enuchsa <enuchsa@gmail.com>
2024-04-02 15:20:35 -0300

    about me section

--- src/app/components/about/about.tsx
+++ src/app/components/about/about.tsx
@@ -1,14 +1,40 @@
+import "./about.css";
+
 export default function About() {
-    return (
-        <section id="about">
-            <p>cu</p>
-            <p>cu</p>
-            <p>cu</p>
-            <p>cu</p>
-            <p>cu</p>
-            <p>cu</p>
-            <p>cu</p>
-            <p>cu</p>
+  return (
+    <section id="about">
+      <section className="about-content">
+        <section className="pic-profile">
+          <img
+            className="pic"
+            src="https://github.com/enuchsa/enuch-portfolio/blob/main/public/assets/images/profile-picture.jpg?raw=true"
+            alt="profile"
+          />
         </section>
-    )
+        <section className="about-text">
+          <h2 className="title-1">
+            About <span>me</span>
+          </h2>
+          <h3 className="title-2">A lover of technology and science</h3>
+          <p>• Graduated in Systems Analysis and Development</p>
+          <p>• Java Software Engineer</p>
+          <p>• Studying AWS and English</p>
+          <p>• B1 Intermediate English Level</p>
+          <p>• Two years of experience</p>
+          <section className="social">
+            <a href="https://www.linkedin.com/in/enuchsa/" target="_blank">
+              <span>
+                <i className="fa-brands fa-linkedin"></i>
+              </span>
+            </a>
+            <a href="https://github.com/enuchsa" target="_blank">
+              <span>
+                <i className="fa-brands fa-github"></i>
+              </span>
+            </a>
+          </section>
+        </section>
+      </section>
+    </section>
+  );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +17,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}
+        <Script src="https://kit.fontawesome.com/21e3de8bee.js" crossOrigin="anonymous"></Script>
+      </body>
     </html>
   );
 }
