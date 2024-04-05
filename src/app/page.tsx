f00a715b8c9b13aa12b155dcc645a27f97c97e51
enuchsa <enuchsa@gmail.com>
2024-04-05 18:05:08 -0300

    refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-import About from "./components/about/about";
-import Experience from "./components/experience/experience";
-import Header from "./components/header/header";
-import Home from "./components/home/home";
-import Project from "./components/project/project";
-import Skill from "./components/skill/skill";
-
 export default function index() {
   return (
     <main>
-      <Header />
-      <Home />
-      <About />
-      <Experience />
-      <Project />
-      {/* <Skill /> */}
+
     </main>
   );
 }
