bb1d32faef88c351352ca85f297c4a05bb74dc3e
enuchsa <enuchsa@gmail.com>
2024-04-09 11:54:34 -0300

    projects ok

--- src/app/pages/project/project.tsx
+++ src/app/pages/project/project.tsx
@@ -8,9 +8,25 @@ export default function Project() {
           <h3>Projects</h3>
           <p>Love building something</p>
         </section>
-
         <section className="content">
-          
+          <section className="item">
+            <h4>Projeto A</h4>
+            <img
+              src="https://th.bing.com/th/id/OIP.1qTMKwdpiGAbQPoIDnr04wHaEK?rs=1&pid=ImgDetMain"
+              alt="projeto x"
+              srcset=""
+            />
+            <p>Este projeteo foi criado para resolver poblema x</p>
+          </section>
+          <section className="item">
+            <h4>Projeto B</h4>
+            <img
+              src="https://th.bing.com/th/id/OIP.1qTMKwdpiGAbQPoIDnr04wHaEK?rs=1&pid=ImgDetMain"
+              alt="projeto x"
+              srcset=""
+            />
+            <p>Este projeteo foi criado para resolver poblema x</p>
+          </section>
         </section>
       </section>
     </section>
